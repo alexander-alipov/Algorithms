2f0c8399f61420118e26bd1775b443792ccd9565
alexander-alipov <stm7i12@gmail.com>
2022-08-11 01:17:19 +0300

    prime numbers

---
+++ prime_numbers.py
@@ -0,0 +1,9 @@
+n=int(input())
+prime = [2,3,5,7,11]
+if n>11:
+    d=sorted(prime+[i for i in range(1,n,2) if i%3!=0 and i%5!=0 and i%7!=0 and i%11!=0])[1:]
+    print(*d)
+else:
+    prime.append(n)
+    prime.sort()
+    print(*prime[:prime.index(n)])
